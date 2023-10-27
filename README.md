f0a730bb2137291ca9e42713977b5a698226c6ea
S4MUR4J <mucha.maciej2000@gmail.com>
2023-10-27 21:51:39 +0200

    Primitive checkboard for test

---
+++ README.md
@@ -0,0 +1,2 @@
+1. [Algorithms Explained – minimax and alpha-beta pruning](https://www.youtube.com/watch?v=l-hh51ncgDI&t=1s)
+2. 
--- main.py
+++ main.py
@@ -1,8 +1,38 @@
+import os
 import math
 import time
+from array import *
+
+# ? 0 - empty
+# ? 1 - Red
+# ? 2 - White
+
+map = [
+        [0, 2, 0, 2, 0, 2, 0, 2],
+        [2, 0, 2, 0, 2, 0, 2, 0],
+        [0, 2, 0, 2, 0, 2, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+    ]
+
+def draw_map():
+    os.system("cls")
+    for i in map:
+        for j in i:
+            print("|" + str(j), end="")
+        print("|")
+
 
 def main():
-    print("Hello World!")
+    while True:
+        draw_map()
+        next_step = input()
+        if next_step == "Q" or next_step == "q":
+            break
+
 
 if __name__ == "__main__":
     main()
