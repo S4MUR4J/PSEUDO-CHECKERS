0d82fc5c33f1ed00d5aa16d1980a74f2d5d6cd35
S4MUR4J <mucha.maciej2000@gmail.com>
2023-10-28 00:01:40 +0200

    Dynamic building map fast implement

--- main.py
+++ main.py
@@ -7,41 +7,56 @@ from array import *
 # ? 'R' - Red
 # ? 'W' - White
 
-map = [
-        [' ', 'W', ' ', 'W', ' ', 'W', ' ', 'W'],
-        ['W', ' ', 'W', ' ', 'W', ' ', 'W', ' '],
-        [' ', 'W', ' ', 'W', ' ', 'W', ' ', 'W'],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        ['R', ' ', 'R', ' ', 'R', ' ', 'R', ' '],
-        [' ', 'R', ' ', 'R', ' ', 'R', ' ', 'R'],
-        ['R', ' ', 'R', ' ', 'R', ' ', 'R', ' '],
-    ]
+n = 8 
 
-def draw_map():
+def init_simulation():
+    map = []
+
+    middle = round(n/2)
+    empty_rows = [middle, middle - 1]
+    if (n % 2 != 0):
+        empty_rows.append(middle + 1)
+
+    for i in range(n):
+        row = []
+        for j in range(n):
+            if (i + j) % 2 == 0 and not any(i in empty_rows for k in empty_rows):
+                if i < n/2:
+                    row.append('W')
+                else:
+                    row.append('R')
+            else:
+                row.append(' ')
+        map.append(row)
+    return map
+
+def draw_visualization(map):
     os.system("cls")
     i = 0
     j = 0
 
     while i < len(map):
         if i == 0:
-            print('    A B C D E F G H \n')
+            k = 0
+            while k < n:
+                print(f' {chr(65 + k)}', end='')
+                k += 1
+            print('\n')
         while j < len(map):
-            if j == 0:
-                print(f"{len(map) - i}  ", end="")
-            print("|" + str(map[i][j]), end="")
+            print("|" + str(map[i][j]), end='')
             j += 1
             if j == len(map):
                 print(f"|  {len(map) - i}")
         j = 0
         i += 1
-        if i == len(map):
-            print('\n    A B C D E F G H')
 
 
 def main():
+    map = init_simulation()
+
     while True:
-        draw_map()
+        draw_visualization(map)
+
         next_step = input()
         if next_step == "Q" or next_step == "q":
             break
